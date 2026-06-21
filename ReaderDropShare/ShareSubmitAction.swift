c4adc9a263859e26589bb29d35400fe62054693d
Kosta <iteaky@gmail.com>
2026-06-21 21:12:59 +0200

    Add share submit action

---
+++ ReaderDropShare/ShareSubmitAction.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+extension ShareSendViewModel {
+    func send() async {
+        guard let file,
+              let destination = URL(string: "https://send2ereader.net") else { return }
+
+        state = .uploading(0)
+        do {
+            let request = UploadRequest(
+                serverURL: destination,
+                key: key,
+                file: file,
+                link: nil,
+                options: options
+            )
+            _ = try await UploadService().upload(request) { value in
+                Task { @MainActor in
+                    self.state = value >= 0.995 ? .processing : .uploading(value)
+                }
+            }
+            state = .success
+        } catch {
+            state = .failure(error.localizedDescription)
+        }
+    }
+}
